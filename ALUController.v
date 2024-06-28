7420d79e873edb34d6768013a43b4b5b9f980563
charconley <conleyw@uci.edu>
2024-06-28 11:48:55 -0400

    Update ALUController.v

--- ALUController.v
+++ ALUController.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/04/2024 12:05:25 PM
-// Design Name: 
-// Module Name: ALUController
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module ALUController(
     ALUOp, Funct7, Funct3, Operation
     );
