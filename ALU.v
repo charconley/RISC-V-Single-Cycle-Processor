7ed3828f680c302a7dbd802b8cb068730043ff96
charconley <conleyw@uci.edu>
2024-06-28 12:14:29 -0400

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,7 +1,5 @@
 `timescale 1ns / 1ps
 
-
-
 module ALU(A_in, B_in, ALU_ctrl, ALU_out, carry_out, zero, overflow);
     input wire [31:0] A_in;
     input wire [31:0] B_in;
@@ -11,21 +9,21 @@ module ALU(A_in, B_in, ALU_ctrl, ALU_out, carry_out, zero, overflow);
     output reg zero = 1'b0;
     output reg overflow = 1'b0;
     always @(*) begin
-        if (ALU_ctrl == 4'b1111) begin
+        if (ALU_ctrl == 4'b1111) begin //equals comparison
             if (A_in == B_in)
                 ALU_out = 32'b1;
             else
                 ALU_out = 32'b0;
         end
-        else if (ALU_ctrl == 4'b1100)
+        else if (ALU_ctrl == 4'b1100) //Nor
             ALU_out = ~(A_in | B_in);
-        else if (ALU_ctrl == 4'b0111) begin
+        else if (ALU_ctrl == 4'b0111) begin //set less than
             if ($signed(A_in) < $signed(B_in))
                 ALU_out = 32'b1;
             else
                 ALU_out = 32'b0;
         end
-        else if (ALU_ctrl == 4'b0110) begin
+        else if (ALU_ctrl == 4'b0110) begin //signed subtraction
             ALU_out = $signed(A_in) - $signed(B_in);
             if ( ($signed(A_in) < 0 && $signed(B_in) > 0) && ALU_out[31] != 1)
                 overflow = 1'b1;
@@ -34,11 +32,11 @@ module ALU(A_in, B_in, ALU_ctrl, ALU_out, carry_out, zero, overflow);
             else
                 overflow = 1'b0;
         end
-        else if (ALU_ctrl == 4'b0000)
+        else if (ALU_ctrl == 4'b0000) //and
             ALU_out = A_in & B_in;
-        else if (ALU_ctrl == 4'b0001)
+        else if (ALU_ctrl == 4'b0001) //or
             ALU_out = A_in | B_in;
-        else begin 
+        else begin  // add
             ALU_out = $signed(A_in) + $signed(B_in);
             if (($signed(A_in) > 0 && $signed(B_in) > 0 && $signed(ALU_out) < 0) || ($signed(A_in) < 0 && $signed(B_in) < 0 && $signed(ALU_out) > 0))
                 overflow = 1'b1;
